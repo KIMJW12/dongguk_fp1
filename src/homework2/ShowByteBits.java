15c8481ec623cac02a13516084e8d64f350f6edd
wldks tls <limelight98@naver.com>
2018-04-10 11:08:56 +0900

    ShowByteBits.java

--- src/homework2/ShowByteBits.java
+++ src/homework2/ShowByteBits.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
-
 public class ShowByteBits {
-    Scanner sc = new Scanner(System.in);
-    byte bv = sc.nextByte();
-       System.out.printf("%d", (bv & 0x80) >> 7); // Byte의 가장 앞 bit를 출력하는 부분
-// 이 사이에 나머지 6bit를 출력하게 만들면 됩니다.
-System.out.printf("%d\n", (bv & 0x01)); // Byte의 가장 뒷 bit를 출력하는 부분
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte bv = sc.nextByte();
+        System.out.printf("%d", (bv & 0x80) >> 7); // Byte의 가장 앞 bit를 출력하는 부분
+        System.out.printf("%d", (bv & 0x40) >> 6);
+        System.out.printf("%d", (bv & 0x20) >> 5);
+        System.out.printf("%d", (bv & 0x10) >> 4);
+        System.out.printf("%d", (bv & 0x08) >> 3);
+        System.out.printf("%d", (bv & 0x04) >> 2);
+        System.out.printf("%d", (bv & 0x02) >> 1);
+                // 이 사이에 나머지 6bit를 출력하게 만들면 됩니다.
+        System.out.printf("%d\n", (bv & 0x01)); // Byte의 가장 뒷 bit를 출력하는 부분
 
+    }
 }
+
+
 
